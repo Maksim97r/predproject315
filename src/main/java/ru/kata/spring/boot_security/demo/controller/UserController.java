8922ca1c5edaa3785430955380cb63827002c470
maksim97r <game452@mail.ru>
2023-04-04 16:04:00 +0300

    final

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -2,20 +2,39 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.security.Principal;
+
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping()
-    public String pageForUserAuthentication(@AuthenticationPrincipal User user, Model model) {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String getUser(Model model, Principal principal) {
+        User user = userService.findByName(principal.getName());
         model.addAttribute("user", user);
         return "user";
     }
+
+
+
+//    @GetMapping()
+//    public String pageForUserAuthentication(@AuthenticationPrincipal User user, Model model) {
+//        model.addAttribute("user", user);
+//        return "user";
+//    }
 }
